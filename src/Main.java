ed462d7eb2e5634861c0f4bab59a648bc34fdac5
hulyayldz <hulya0898yildiz@gmail.com>
2025-04-27 16:58:16 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import enums.BreadRollType;
+import enums.CipsType;
+import enums.DrinkType;
+import model.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hamburger hamburger = new Hamburger("Basic","NORMAL",3.56, BreadRollType.WRAP);
+        hamburger.addAddition("Tomato",0.30);
+        hamburger.addAddition("Lettuce",0.9);
+        hamburger.addAddition("Cheese",1.25);
+        hamburger.addAddition("Pickle",0.42);
+        hamburger.itemizeHamburger();
+
+
+        HealthyBurger healthyBurger =new HealthyBurger("Vegan Burger",5.67,BreadRollType.SANDWICH);
+        healthyBurger.addAddition("Falafel",5.25);
+        healthyBurger.itemizeHamburger();
+
+        DeluxeBurger db = new DeluxeBurger(new Drink(DrinkType.FANTA),new Cips(CipsType.CURLY));
+        db.addAddition("dummy",40);
+        db.itemizeHamburger();
     }
+
 }
